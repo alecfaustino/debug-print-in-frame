07d1b0789b3525ff24d86b050dc38ee2a106da8b
Alec Faustino <alecfaustino@outlook.com>
2025-03-09 14:35:22 -0600

    completed debug of printInFrame

--- printInFrame.js
+++ printInFrame.js
@@ -1,17 +1,18 @@
 const printInFrame = function(list) {
-  list = list.split(' ');
+  list = list.split(' '); // is an array of each word
   const longest = longestStr(list).length;
-  const border = repeat('*', longest);
+  const border = repeat('*', longest + 3); // because there's a * and a ' ' to start each line, there should be a star to make up for those. 
 
-  console.log(border);
+  console.log(border); // top border
   for (const word of list) {
-    console.log(`* ${word}${repeat(' ', longest - word.length + 1)}*`);
+    // adds a * at the beginning and the end. fills space in the middle so all lines are equal to the longest word
+    console.log(`* ${word}${repeat(' ', longest - word.length)}*`);
   }
-  console.log(border);
+  console.log(border); // bottom border 
 };
 
 const repeat = function(str, times) {
-  let result = str;
+  let result = str; // default starts with 1 instance of the string then concats extras
 
   for (let i = 0; i < times; i++) {
     result += str;
@@ -24,7 +25,9 @@ const longestStr = function(list) {
   let longest = list[0];
 
   for (const str of list) {
+    if(str.length > longest.length) { // add this to check if it's longer first, rather than just assigning it str every time. Otherwise, longest is always the last word. 
     longest = str;
+    }
   }
 
   return longest;
